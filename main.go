c2fcc46f052cbcf4c4e2cf02fc48f8c704bfb3a3
Rakesh <rakesh.rakshit@kartbites.com>
2019-07-19 15:12:03 +0530

    Markdown demo dependency

--- main.go
+++ main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
-
-	router.Run(":" + port)
+	r.Run(":" + "8080")
 }
